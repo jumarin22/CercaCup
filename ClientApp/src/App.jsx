162b29d517b8af3f86d467134186923c94c3d78b
Justin Marin <jumarin@protonmail.com>
2021-06-14 19:13:48 -0400

    Search page HTML skeleton

--- ClientApp/src/App.jsx
+++ ClientApp/src/App.jsx
@@ -3,6 +3,20 @@ import './custom.scss'
 
 export function App() {
   return (
-    <div className="d-flex justify-content-center display-2">Hello, World!</div>
+    <>
+      <h1>☕</h1>
+      <h1>CercaCup</h1>
+      <p className="subtitle">Find the closest cup of coffee near you!</p>
+      <form>
+        <input type="text" placeholder="Enter zipcode" />
+        <br />
+        <input type="checkbox" />
+        <label htmlFor="vehicle1">Coffee Shops </label>
+        <input type="checkbox" />
+        <label htmlFor="vehicle2">Gas Stations </label>
+        <input type="checkbox" />
+        <label htmlFor="vehicle3">Fast Food </label>
+      </form>
+    </>
   )
 }
