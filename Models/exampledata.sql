26687b14752c3ed5c7bc1ba638f9da2e09f2b7f3
Justin Marin <jumarin@protonmail.com>
2021-06-16 15:55:27 -0400

    example data loaded into database

---
+++ Models/exampledata.sql
@@ -0,0 +1,5 @@
+TRUNCATE TABLE "Locations";
+
+INSERT INTO "Locations" ("Name", "Type", "Address") VALUES ('Cecile Bakery + Cafe', 'Cafe', '5744 Sunset Dr');
+INSERT INTO "Locations" ("Name", "Type", "Address") VALUES ('Chevron', 'Gas Station', '6180 Sunset Dr');
+INSERT INTO "Locations" ("Name", "Type", "Address") VALUES ('Wendy''s', 'Fast Food', '6898 Bird Rd');
